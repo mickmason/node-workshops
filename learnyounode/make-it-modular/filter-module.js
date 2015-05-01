e10d4885db4c9c7edc77c827f9ac3c03e9c473d5
mickmason <mmason2@hotmail.com>
2015-05-01 18:26:06 +0100

    Adding files after make it modular exercise

--- learnyounode/make-it-modular/filter-module.js
+++ learnyounode/make-it-modular/filter-module.js
@@ -1,19 +1,30 @@
 module.exports = function(dirname, filter, callback) {
-	//console.log(typeof args) ;	
+	//console.log(dirname) ;	
+	
+	
+		//console.log("dirname: "+dirname) ;
+		// console.log("filter: "+filter) ;
+		// console.log("callback: "+callback) ;
+	
 	var fs = require('fs') ;
 	var path = require('path') ;
 	var data = [] ;
 	fs.readdir(dirname, function(err, files) {
 		if (err) 
-			return callback(err) ;
-		
+			return callback(err, data) ;
+		var count = 0;
 		files.forEach(function(file, i) {
+				
 			if (path.extname(file).toLowerCase() === "." + filter.toLowerCase()) {
-							console.log(file) ;
-							data[i] = file ;
-			}
+		
+		
+				data[count] = file ;
+				count++;
+			} 
 		});
-		return callback(err, data) ;
+			
+		return callback(null, data) ;	
 	});
+
 
 };
--- learnyounode/make-it-modular/filtered-ls.js
+++ learnyounode/make-it-modular/filtered-ls.js
@@ -1,14 +1,17 @@
 
 var filterMod = require('./filter-module.js') ;
-// console.log(process.argv[1]+process.argv[2]+process.argv[3])
-filterMod(process.argv[2], process.argv[3], process.argv[4]);
-// var cb = function(err, data) {
-// 	if (err){
-// 			console.log(err) ;
-// 	} else {
-// 		data.forEach(function(file) {
-// 			console.log(file) ;
-// 		}) ;
-// 	}
-// }
-// filterMod("C:\\Users\\Michael\\Work\\JavaScript\\NodeJS\\node-workshops\\learnyounode\\make-it-modular", "js", cb);
+
+var cb = function(err, data) {
+	if (err) {
+		console.log(err) ;
+		return false;
+	} else if (data.length < 1){
+		console.log("Sorry, no results")
+	} else {
+		data.forEach(function(el, i) {console.log(el)}) ;
+	}
+}
+
+filterMod(process.argv[2], process.argv[3], cb);
+
+
