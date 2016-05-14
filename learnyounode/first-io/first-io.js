5074735a4f0e64c6e9b3e652f3421236cbb59c34
mickmason <mmason2@hotmail.com>
2016-05-14 05:32:36 +0100

    getting back to this, working through early examples

--- learnyounode/first-io/first-io.js
+++ learnyounode/first-io/first-io.js
@@ -1,9 +1,6 @@
-var fs = require('fs') ;
+var fs = require('fs');
+var data = fs.readFileSync(process.argv[2]);
+data = data.toString();
+var dataSplit = data.split('\n');
+console.log(dataSplit.length-1);
 
-var buff = fs.readFileSync(process.argv[2], {'encoding': 'ascii'});
-var newlines = 0;
-for (var i = 0; i<buff.length; i++) {
-	if (buff.charAt(i) === '\n') 
-		newlines += 1;
-}
-console.log(newlines) ;
