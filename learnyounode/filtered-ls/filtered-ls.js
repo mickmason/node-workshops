3462352551ad7064d87502c49b7b7e8dae6ebc03
mickmason <mmason2@hotmail.com>
2016-05-14 15:33:42 +0100

    More learning 14th May

--- learnyounode/filtered-ls/filtered-ls.js
+++ learnyounode/filtered-ls/filtered-ls.js
@@ -1,12 +1,8 @@
-var fs = require('fs') ;
-var path = require('path') ;
+var fs = require('fs');
+var path = require('path');
+var filter = process.argv[3];
 fs.readdir(process.argv[2], function(err, files) {
-	if (err) console.log(err) ;
-	var extn =  process.argv[3] ;
-	files.forEach(function(file) {
-		if (path.extname(file).substr(path.extname(file).search(".")+1).toLowerCase() === extn)
-			console.log(file) ;	
-	});
-
-	
+	(err) ? console.log(err) :  files.forEach(function(file) {
+		(path.extname(file) === '.'+filter) ? console.log(file) : true ;
+	}); 
 });
