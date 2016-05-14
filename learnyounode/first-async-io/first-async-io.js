c84276397819cced7c11998aab6f12dbf88ad93c
mickmason <mmason2@hotmail.com>
2016-05-14 13:22:50 +0100

    learning 14th May

--- learnyounode/first-async-io/first-async-io.js
+++ learnyounode/first-async-io/first-async-io.js
@@ -1,12 +1,7 @@
-var fs = require('fs') ;
+var fs = require('fs');
+var count = 0;
 
-fs.readFile(process.argv[2], {'encoding': 'ascii'}, function(err, data) {
-	if (err) console.log(err)
-	var buff = data;	
-	var newlines = 0;
-	for (var i = 0; i<buff.length; i++) {
-		if (buff.charAt(i) === '\n') 
-			newlines += 1;
-	}
-	console.log(newlines) ;	
+fs.readFile(process.argv[2], function(err, data){
+	(err) ? console.log(err) : console.log(data.toString().split('\n').length-1);
 });
+
